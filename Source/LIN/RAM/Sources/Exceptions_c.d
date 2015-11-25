9b070f4e15e76afe51e747b43f8ecce4d0ffdf26
hugolCervantes <os_decerv11_ipn@hotmail.com>
2015-11-25 10:34:35 -0600

    Add the first folders for start to work in Lin, only documentation for the practice.

---
+++ Source/LIN/RAM/Sources/Exceptions_c.d
@@ -0,0 +1,6 @@
+Sources/Exceptions_c.obj: C:\Users\Isa\Documents\LIN_AEP.git\trunk\Source\LIN\Sources\Exceptions.c \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\lib_ewl.prefix \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\common.prefix \
+	C:\Freescale\CW\ MCU\ v10.6\MCU\PA_Support\ewl\EWL_C\include\ewl_c_version.h \
+	C:\Users\Isa\Documents\LIN_AEP.git\trunk\Source\LIN\Prefix\MPC5606B_RAM_VLE.prefix \
+	C:\Users\Isa\Documents\LIN_AEP.git\trunk\Source\LIN\Project_Headers\Exceptions.h 
